cecaa94e638649a4c6f675f8d3671c6043db3639
creio <mailcreio@gmail.com>
2024-04-08 10:23:55 +0300

    08.04.2024 Update

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,14 @@
 # @Last Modified time: 19.05.2022 09:45
 
 repo_name=ctlos-aur
-local_repo=/media/files/github/ctlos/ctlos-aur/x86_64
-srht_repo=/media/files/srht/ctlos/ctlos-aur/x86_64
+arch=x86_64
+local_repo=/media/files/github/ctlos/ctlos-aur/
+srht_repo=/media/files/srht/ctlos/ctlos-aur/
 repo_osdn=creio@storage.osdn.net:/storage/groups/c/ct/ctlos/ctlos-aur/
 repo_keybase=/run/user/1000/keybase/kbfs/public/cvc/ctlos-aur/
 
 if [[ -d "$repo_keybase" ]]; then
-  rsync -avrCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo"/ "$repo_keybase"
+  rsync -avrCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo/$arch/" "$repo_keybase"
 fi
 # rsync -avrCLP --delete-excluded --delete "$local_repo" "$repo_osdn"
 echo "add pkg, rsync all repo"
@@ -23,15 +24,18 @@ git_up() {
   git push
 }
 
-cd $local_repo
+cd $local_repo/$arch
 ## fix github symlink
 rm $repo_name.{db,files}
 cp -f $repo_name.db.tar.gz $repo_name.db
 cp -f $repo_name.files.tar.gz $repo_name.files
 cd ..
-cp -rfv $local_repo $srht_repo
 git_up
-cd $srht_repo
-git_up
+
+if [[ -d "$srht_repo" ]]; then
+  rsync -avrCLP --delete --exclude={"build",".git*"} "$local_repo" "$srht_repo"
+  cd $srht_repo
+  git_up
+fi
 
 echo -e "\033[0;32mDeploy repo Done\033[0m"
